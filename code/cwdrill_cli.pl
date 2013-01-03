5533205f7cf3c291a36cb67f9811384700a977b3
Josh Myer <josh@joshisanerd.com>
2013-01-03 11:14:10 -0800

    cwdrill: adds really jeanky first pass at an exercise-bike-friendly CW drill program

---
+++ code/cwdrill_cli.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+# Requires cwwav in PATH; https://github.com/Kerrick/cwwav/archive/master.tar.gz
+
+my $t = `mktemp`;
+chomp($t);
+
+$t = $t . ".wav";
+
+print "Created tempfile $t <-- here\n";
+
+while(1) {
+    print `clear`;
+    my $alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; # 0123456789";
+
+    my $i = int(rand(length($alphabet)));
+
+    my $x = substr($alphabet, $i, 1);
+
+    `echo "$x $x" | cwwav -f 700 -w 5 -o $t &> /dev/null`;
+    `mplayer $t &> /dev/null`;
+    sleep 1;
+    print `banner "     $x" `;
+    sleep 4;
+}
